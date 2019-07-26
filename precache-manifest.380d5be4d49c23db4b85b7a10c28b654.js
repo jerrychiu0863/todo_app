83e84cb1cae78e8c2938a253a65add9e50092d2c
Jerry <m08632000@hotmail.com>
2019-07-26 11:29:25 +0800

    Updates

---
+++ precache-manifest.380d5be4d49c23db4b85b7a10c28b654.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2218475503c1255d8286f486cf97f0c5",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "2fabf354b8cd6b9a2145",
+    "url": "/todo_app/static/css/main.24fe4e91.chunk.css"
+  },
+  {
+    "revision": "91cb2147a924400a5628",
+    "url": "/todo_app/static/js/2.8ce80226.chunk.js"
+  },
+  {
+    "revision": "2fabf354b8cd6b9a2145",
+    "url": "/todo_app/static/js/main.1a753610.chunk.js"
+  },
+  {
+    "revision": "6c1ee6e246643c8a5a1d",
+    "url": "/todo_app/static/js/runtime~main.da743e9d.js"
+  }
+]);
