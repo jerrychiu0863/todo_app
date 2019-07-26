47842f4a294f2d3faa7b60678fce0e84b944db13
Jerry <m08632000@hotmail.com>
2019-07-26 11:32:52 +0800

    add badsename on router and ghpages

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <App />
     </Router>
   </Provider>,
